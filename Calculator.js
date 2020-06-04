d92c52c59bfc01f1013613370ece97557bf79391
ANGELperfect <62509819+ANGELperfect@users.noreply.github.com>
2020-06-04 11:20:32 +0100

    Update Calculator.js

--- Calculator.js
+++ Calculator.js
@@ -1,17 +1,17 @@
 var dark = false;
 var btn = $("#dark");
 $(document).ready(function(){
-    alert("This is just a simple calculator not scientific and can't do complicated operations , only basic ones. \n Enjoy (^_^) !\n");
-    $("div").slideDown(function(){$("#dark").animate({opacity:"100%"});});
+    // alert("This is just a simple calculator not scientific and can't do complicated operations , only basic ones. \n Enjoy (^_^) !\n");
+    $("div").fadeIn(5000,function(){$("#dark").animate({opacity:"100%"});});
     btn.click(function(){
         if (dark === false){
             btn.text("Light mode");
-            document.getElementById('theme').setAttribute("href","Darkmode.css");
+            $("#theme").attr("href","Darkmode.css");;
             dark = true;
         }
         else{
             btn.text("Dark mode");
-            document.getElementById('theme').setAttribute("href","Calculator.css");
+           $("#theme").attr("href","Calculator.css");
             dark = false;
         }
 
@@ -27,7 +27,7 @@ function insert(num){
 
 function equal(){
     document.form.textView.value = eval( document.form.textView.value);
-    if (eval( document.form.textView.value) === Infinity){document.form.textView.value = "N3iK  -_-";}
+    if (eval( document.form.textView.value) === Infinity){document.form.textView.value = "  Can't divid  by 0";}
     if (eval( document.form.textView.value) === undefined){document.form.textView.value = "";}
 }
 
@@ -40,6 +40,13 @@ function back(){
    document.form.textView.value = text.substring(0,text.length-1);
 }
 
+
+// var logo = document.querySelectorAll("#logo path");
+// console.log(logo);
+
+// for (let i =0 ; i<logo.length ; i++){
+//     console.log("letter "+i+" is "+logo[i].getTotalLength());
+// }
 
 
 
